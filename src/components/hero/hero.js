d489e69a286b30913f74da5bbac577cffbe4a837
kubanychhbekova <a7966519@gmail.com>
2023-07-06 11:20:14 +0600

    finish hero

---
+++ src/components/hero/hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div id="hero">
+            <div className="container">
+                <div className="hero">
+                    <div className='hero--parent'>
+                        <h1 >Welcome to our Bookshop</h1>
+                        <p>A place where you can get all the books you want!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
