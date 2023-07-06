bef0af04911e2288d6a8c5715214988702f4c7a6
kubanychhbekova <a7966519@gmail.com>
2023-07-06 16:54:27 +0600

    finish slider

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,8 +1,13 @@
-const initialState={
-    basket:[],
-    filter:[],
-    product:[],
+const initialState = {
+    basket: [],
+    filter: [],
+    product: [],
 }
-export const Reducer=(state=initialState,action)=>{
-
+export const Reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "GET_PRODUCT":
+            return {...state, product: action.payload}
+        default :
+            return state
+    }
 }
