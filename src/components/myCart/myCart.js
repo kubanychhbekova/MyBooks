668c0061c9dd0942a3d0ad1914d21a631e860f2e
nursdeveloper <amanturovnursultan458@gmail.com>
2023-07-07 10:37:26 +0300

    myCart-starting

--- src/components/myCart/myCart.js
+++ src/components/myCart/myCart.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Cart = () => {
     return (
-        <div>
-            
+        <div id='myCart'>
+            <div className="container">
+                <div className="myCart">
+                    <div className="myCart--yourCart">
+                        <div className="myCart--yourCart__zagalovok">
+                            <h1>Your cart</h1>
+                            <p>Not ready to checkout? <span>
+                                    <NavLink to={'/'}>Continue Shopping</NavLink>
+                                </span></p>
+                        </div>
+                        <div className="myCart--basket">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
